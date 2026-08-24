736cb2680a7edf387756c488cae903457ecdc98d
Lucifer <151667791+Hemant-jindal708@users.noreply.github.com>
2026-08-24 21:41:29 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int> freq;
+        for(int i:nums)
+        {
+            if(freq[i]!=0)
+            {
+                return true;
+            }
+            else
+                freq[i]++;
+        }
+        return false;
+    }
+};
